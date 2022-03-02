361b86dec906690edd4150b25c955ec7176e2326
hik0 <2hiko5dayo@gmail.com>
2022-03-02 14:48:07 +0900

    [Add] index

---
+++ ABC241_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int iIn[10];
+  for(int i = 0; i < 10; ++i)
+  {
+    cin >> iIn[i];
+  } 
+  cout << iIn[iIn[iIn[0]]] << endl;
+}
