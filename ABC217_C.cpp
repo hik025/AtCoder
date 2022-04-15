3eca4e8e442615f91a199be920686af08fb2e25a
hik0 <2hiko5dayo@gmail.com>
2022-04-15 15:50:23 +0900

    [Add] index

---
+++ ABC217_C.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<vector<int>> v(n, vector<int>(2));
+	for (int i = 0; i < n; ++i){
+		cin >> v[i][0];
+		v[i][1] = i + 1;
+	}
+
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; ++i){
+		cout << v[i][1];
+		if (n - 1 != i) cout << " ";
+		else cout << endl;
+	}
+}
